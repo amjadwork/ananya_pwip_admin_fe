5c6211a48248e25fb2611bbd703b75b19bd57788
Sudhanshu Kumar Swain <sudhanshu@i-darts.com>
2023-06-12 15:56:05 +0530

    component folders

---
+++ src/components/common/Select/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Select as MantineSelect } from "@mantine/core";
+
+function Select(props: any) {
+  return <MantineSelect {...props} />;
+}
+
+export default Select;
