c96aee6913aba6071efe1e486ab8eeb3685e3679
Sudhanshu Kumar Swain <sudhanshu@i-darts.com>
2023-06-12 16:03:08 +0530

    component folders

--- src/components/common/Alert/AlertError.tsx
+++ src/components/common/Alert/AlertError.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Alert } from "@mantine/core";
 
+const AlertError = (props: any) => {
+  return (
+    <Alert title="Something wrong" color="red" radius="md">
+      API'S SHOWING ERROR.
+    </Alert>
+  );
+};
 
-
-const AlertError =(props:any)=>{
-    return(
-          <Alert title="Something wrong" color="red" radius="md">
-              API'S SHOWING ERROR.
-            </Alert>
-           );
-}
 export default AlertError;
