afdfa34df2b1baeceb8322403888c54eae2b3475
Ananya Singh <ananya@pwip.co>
2023-06-23 13:02:50 +0530

    useEffect used to manage filteredData

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,7 +1,6 @@
-import React,{useState} from 'react';
-import {ScrollArea, Table, Box, Pagination,Group} from "@mantine/core";
- import {Input} from "../../components"
-
+import React, { useEffect, useState, useMemo } from "react";
+import { ScrollArea, Table, Box, Pagination, Group } from "@mantine/core";
+import { Input } from "../../components";
 
 interface DataTableProps {
   dataCopy: any[];
@@ -9,14 +8,23 @@ interface DataTableProps {
 }
 
 const DataTable = (props: DataTableProps) => {
-    const { dataCopy, destinationSelectOptions } = props;
-    const [searchQuery, setSearchQuery] = useState('');
-    const [filteredData, setFilteredData] = useState(dataCopy);
-    const [activePage, setPage] = useState(1)
+  const { dataCopy, destinationSelectOptions } = props;
 
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredData, setFilteredData] = useState<any>([]);
+  const [activePage, setActivePage] = useState(1);
+  const dataPerPage = 10;
 
+  useEffect(() => {
+    const startIndex = (activePage - 1) * dataPerPage;
+    const endIndex = startIndex + dataPerPage;
+    setFilteredData([...dataCopy].slice(startIndex, endIndex));
+  }, [dataCopy, activePage]);
 
-const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const setPage = (page: number) => {
+    setActivePage(page);
+  };
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const query = event.target.value;
     setSearchQuery(query);
     const filtered = dataCopy.filter((item) =>
@@ -25,78 +33,78 @@ const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFilteredData(filtered);
   };
 
-  console.log("d", destinationSelectOptions)
+  const rows = useMemo(()=>{
+  return filteredData.map((item: any) => (
+    <React.Fragment key={item.name}>
+      {item.list.map((listItem: any) => {
+        const destinationName = destinationSelectOptions.find(
+          (f: any) => f.value === listItem._destinationPortId
+        )?.label;
 
-  const rows = filteredData.map((item:any) =>(
-      <React.Fragment key={item.name}>
-        {item.list.map((listItem: any) => {
-          const destinationName = destinationSelectOptions.find(
-            (f: any) => f.value === listItem._destinationPortId
-          )?.label;
-  
-          return (
-            <tr key={`${item.name}-${listItem._destinationPortId}`}>
-                <td>{item.name}</td>
-              <td>{destinationName}</td>
-              <td>{listItem.chaCharge}</td>
-              <td>{listItem.silicaGel}</td>
-              <td>{listItem.craftPaper}</td>
-              <td>{listItem.transportCharge}</td>
-              <td>{listItem.customCharge}</td>
-              <td>{listItem.loadingCharge}</td>
-              <td>{listItem.coo}</td>
-            </tr>
-          );
-        })}
-      </React.Fragment>
-    ));
-  
-    return (
-        <Box>
-        <Input
-          style={{marginBottom:"8px"}}
-          type="text"
-          placeholder="Search by Origin Port"
-          value={searchQuery}
-          onChange={handleSearchChange}
-        />
-      <Table 
-      highlightOnHover 
-      withBorder 
-      withColumnBorders>
-
-        {/* <thead style={{ position: "sticky", top: 0}}> */}
-        <ScrollArea w={890} h={400} >
-        <thead style={{ position: "sticky", top:0, backgroundColor:"#666666"}}>
-          <tr>
-            <th style={{ opacity:1, color: "#fffff"}}>Origin</th>
-            <th>Destination</th>
-            <th>CHA</th>
-            <th>SilicaGel</th>
-            <th>CraftPaper</th>
-            <th>Transport</th>
-            <th>Custom</th>
-            <th>Loading</th>
-            <th>COO</th>   
+        return (
+          <tr key={`${item.name}-${listItem._destinationPortId}`}>
+            <td>{item.name}</td>
+            <td>{destinationName}</td>
+            <td>{listItem.chaCharge}</td>
+            <td>{listItem.silicaGel}</td>
+            <td>{listItem.craftPaper}</td>
+            <td>{listItem.transportCharge}</td>
+            <td>{listItem.customCharge}</td>
+            <td>{listItem.loadingCharge}</td>
+            <td>{listItem.coo}</td>
           </tr>
-        </thead>
-        
-        <tbody  style={{
-       width: "100%",
-       borderCollapse: "collapse",
-       tableLayout: "fixed",
-       }} >{rows}</tbody>
+        );
+      })}
+    </React.Fragment>
+  ));
+ }, [filteredData, destinationSelectOptions]); 
+
+  return (
+    <Box>
+      <Input
+        style={{ marginBottom: "8px" }}
+        type="text"
+        placeholder="Search by Origin Port"
+        value={searchQuery}
+        onChange={handleSearchChange}
+      />
+      <Table highlightOnHover withBorder withColumnBorders>
+        <ScrollArea w={890} h={400}>
+          <thead
+            style={{ position: "sticky", top: 0, backgroundColor: "#666666" }}>
+            <tr>
+              <th style={{ opacity: 1, color: "#fffff" }}>Origin</th>
+              <th>Destination</th>
+              <th>CHA</th>
+              <th>SilicaGel</th>
+              <th>CraftPaper</th>
+              <th>Transport</th>
+              <th>Custom</th>
+              <th>Loading</th>
+              <th>COO</th>
+            </tr>
+          </thead>
+
+          <tbody
+            style={{
+              width: "100%",
+              borderCollapse: "collapse",
+              tableLayout: "fixed",
+            }}>
+            {rows}
+          </tbody>
         </ScrollArea>
       </Table>
-      <Group 
-      style={{margin:"5px"}} >
-        <Pagination 
-        total={10} 
+      <Group style={{ margin: "5px" }}>
+        <Pagination
+        total={dataCopy.length}
+        // value={10}
+        page={activePage}
         color="gray"
-        />
-      </Group> 
-      </Box>
-    );
-  };
+        onChange={setPage} />
+      </Group>
+    </Box>
+  );
+};
 
 export default DataTable;
