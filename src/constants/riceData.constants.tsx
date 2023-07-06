c7ec6678965cdd204ed8622186e0f58f31438746
Ananya Singh <ananya@pwip.co>
2023-07-06 12:44:29 +0530

    variantData dummy

---
+++ src/constants/riceData.constants.tsx
@@ -0,0 +1,178 @@
+export const riceData=[
+    {
+     _categoryId:"641e0f2645fe91930399b09f",
+     _productId:"641e0f2545fe91930399b09b",
+     variantName:"IR64 Raw Fresh Milling",
+     sourceRates:[
+        {
+            sourceName:"Raipur",
+            _sourceId:"643d027a2a4349329f428bec",
+            price:34,
+            unit:"Kg",
+        },
+        {
+            sourceName:"Allahabad",
+            _sourceId:"643d027a2a4349329f428bec",
+            price:27,
+            unit:"Kg",
+        },
+        {
+            sourceName:"Punjab",
+            _sourceId:"643d027a2a4349329f428bec",
+            price:25,
+            unit:"Kg",
+        },
+        {
+            sourceName:"Karnal",
+            _sourceId:"643d027a2a4349329f428bec",
+            price:56,
+            unit:"Kg",
+        },
+        {
+            sourceName:"Haryana",
+            _sourceId:"643d027a2a4349329f428bec",
+            price:23,
+            unit:"Kg",
+        },
+        {
+            sourceName:"Nellore",
+            _sourceId:"643d027a2a4349329f428bec",
+            price:231,
+            unit:"Kg",
+        },
+      ],
+
+    },
+
+    {
+        _id:"64a5bb877fd50f68bd46d465",
+        _categoryId:"641e0f2645fe91930399b09e",
+        _productId:"641e0f2545fe91930399b09b",
+        variantName:"1121 Basmati Golden Sella",
+        sourceRates:[
+           {
+               sourceName:"Kanpur",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:34,
+               unit:"Kg",
+
+
+           },
+           {
+               sourceName:"Allahabad",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:27,
+               unit:"Kg",
+
+
+           },
+           {
+               sourceName:"Lucknow",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:25,
+               unit:"Kg",
+
+
+           },
+           {
+               sourceName:"Karnal",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:56,
+               unit:"Kg",
+
+
+           },
+           {
+               sourceName:"Haryana",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:23,
+               unit:"Kg",
+
+
+           },
+           {
+               sourceName:"Nellore",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:231,
+               unit:"Kg",
+
+
+           },
+         ],
+   
+       },
+       {
+        _id:"64a5bb877fd50f68bd46d465",
+        _categoryId:"641e0f2645fe91930399b09f",
+        _productId:"641e0f2545fe91930399b09b",
+        variantName:"Idly Rice ADT-37",
+        sourceRates:[
+           {
+               sourceName:"Kanpur",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:211,
+               unit:"Kg",
+
+
+           },
+           {
+               sourceName:"Allahabad",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:88,
+               unit:"Kg",
+
+
+           },
+           {
+               sourceName:"Lucknow",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:666,
+               unit:"Kg",
+
+
+           },
+           {
+               sourceName:"Karnal",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:93.3,
+               unit:"Kg",
+
+
+           },
+           {
+               sourceName:"Haryana",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:23.4,
+               unit:"Kg",
+
+
+           },
+           {
+               sourceName:"Nellore",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:21.1,
+               unit:"Kg",
+
+
+           },
+         ],
+   
+       },
+       {
+        _id:"64a5bb877fd50f68bd46d465",
+        _categoryId:"641e0f2645fe91930399b09e",
+        _productId:"641e0f2545fe91930399b09b",
+        variantName:"1121 Basmati Golden Sella",
+        sourceRates:[
+           {
+               sourceName:"Burdwan",
+               _sourceId:"643d027a2a4349329f428bec",
+               price:34.5,
+               unit:"Kg",
+               _id:"64a5bb877fd50f68bd46d466"
+           },
+        ],
+       },
+]
+
+
