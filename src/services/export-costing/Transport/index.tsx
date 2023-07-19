e44cee54e1d210c4512bdae8d75af3c2c301179b
Ananya Singh <ananya@pwip.co>
2023-07-19 07:41:05 +0000

    CU-85zthc1fq_FE-Admin-Transport-Cost-Management

--- src/services/export-costing/Transport/index.tsx
+++ src/services/export-costing/Transport/index.tsx
@@ -1,6 +1,6 @@
 import APIRequest from "../../../helper/api";
 
-export const getTransportData = async () => {
+export const getTransportationData = async () => {
   const response: any = await APIRequest("transportation", "GET");
   if (response) {
     return response;
@@ -8,26 +8,49 @@ export const getTransportData = async () => {
 };
 
 export const getSourceData = async () => {
-  const response = await APIRequest("location?filterType=source", "GET");
+  const response = await APIRequest("location/source", "GET");
   if (response) {
     return response;
   }
 };
 
 export const getOriginData = async () => {
-  const response = await APIRequest("location?filterType=origin", "GET");
+  const response = await APIRequest("location/origin", "GET");
   if (response) {
     return response;
   }
 };
 
-export const postTransportData = async (transportAPIPayload: any) => {
+export const postTransportationData = async (data: any) => {
   const response = await APIRequest(
     "transportation",
     "POST",
-    transportAPIPayload
+    data
   );
   if (response) {
     return response;
   }
 };
+
+export const deleteTransportationData = async (data: any) => {
+  const response = await APIRequest(
+    "transportation" + "/" + data._originPortId + "/" + data._id,
+    "DELETE"
+  );
+  if (response) {
+    return response;
+  }
+};
+
+export const patchTransportationData = async (data: any) => {
+  const response = await APIRequest(
+    "transportation" + "/" + data[0]._originPortId
+    + "/" + data[0]._transportObjId,
+    "PATCH", data[0]);
+  if (response) {
+    console.log(response)
+    return response;
+  }
+};
+
+
