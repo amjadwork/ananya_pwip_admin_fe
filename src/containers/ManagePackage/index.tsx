915cb241d1036c87451ac82c13e307cbd8fb3fdd
Ananya Singh <ananya@pwip.co>
2023-07-20 10:27:35 +0000

    CU-85zthc1ta_FE-Admin-Packaging-Management

--- src/containers/ManagePackage/index.tsx
+++ src/containers/ManagePackage/index.tsx
@@ -1,163 +1,150 @@
-import React, { useEffect } from "react";
-import {Plus } from "tabler-icons-react";
-import {Text} from "../../components/index";
+import React, {useEffect,useState} from "react";
+import { Plus, X , Check} from "tabler-icons-react";
+import { Text} from "../../components/index";
 import { openConfirmModal } from "@mantine/modals";
+import { showNotification } from "@mantine/notifications";
 
-import EditPackageForm from "../../forms/ManagePackage";
+import EditPackageForm from "../../forms/ManagePackage/index";
 import PageWrapper from "../../components/Wrappers/PageWrapper";
 import DataTable from "../../components/DataTable/DataTable";
 import APIRequest from "../../helper/api";
-
+import {
+  // getPackagingData,
+  postPackagingData,
+  deletePackagingData,
+  putPackagingData,
+} from "../../services/export-costing/Packaging";
 
 const columns = [
-  {
-    label: "Bag Type",
-    key: "bag",
-    sortable: true,
-  },
-  {
-    label: "Weight(in Kgs)",
-    key: "weight",
-  },
-  {
-    label: "Cost(in INR)",
-    key: "cost",
-  },
-  {
-    label: "Action",
-    key: "action",
-  },
-];
+    {
+      label: "Bag Type",
+      key: "bag",
+      sortable: true,
+    },
+    {
+      label: "Weight(in Kgs)",
+      key: "weight",
+    },
+    {
+      label: "Cost(in INR)",
+      key: "cost",
+    },
+    {
+      label: "Action",
+      key: "action",
+    },
+  ];
 
-const initialBagTypes = [
-  {
-    type: "PPWOVEN",
-    list: [],
-  },
-  {
-    type: "JUTE",
-    list: [],
-  },
-  {
-    type: "PE",
-    list: [],
-  },
-  {
-    type: "BOPP",
-    list: [],
-  },
-];
+  const initialBagTypes = [
+      {
+        type: "PPWOVEN",
+        list: [],
+      },
+      {
+        type: "JUTE",
+        list: [],
+      },
+      {
+        type: "PE",
+        list: [],
+      },
+      {
+        type: "BOPP",
+        list: [],
+      },
+    ];
 
 const RenderModalContent = (props: any) => {
   const handleCloseModal = props.handleCloseModal;
-  const handleSaveCallback = props.handleSaveCallback;
   const updateFormData = props.updateFormData;
-  const modalType=props.modalType;
-  const modalOpen = props.modalOpen;
+  const handleSaveAction = props.handleSaveAction;
+  const modalType = props.modalType;
 
   return (
     <EditPackageForm
       handleCloseModal={handleCloseModal}
-      handleSaveCallback={handleSaveCallback}
+      handleSaveAction={handleSaveAction}
       updateFormData={updateFormData}
       modalType={modalType}
-      modalOpen={modalOpen}
     />
   );
 };
 
 function ManagePackageContainer() {
-  const [modalOpen, setModalOpen] = React.useState<any>(false);
-  const [modalType, setModalType] = React.useState<string>("add");
-  const [packagingList, setPackagingList] = React.useState([
-    ...initialBagTypes,
-  ]);
-  const [tableRowData, setTableRowData] = React.useState<any>([]);
+  const [modalOpen, setModalOpen] = useState<any>(false);
+  const [modalType, setModalType] = useState<string>("add");
+  const [packagingData, setPackagingData] = useState<any>([]);
+  const [updateFormData, setUpdateFormData] = useState<any>(null);
   const [selectedTableRowIndex, setSelectedTableRowIndex] =
-  React.useState<any>(null);
-  const [updateFormData, setUpdateFormData] = React.useState<any>(null);
+  useState<any>(null);
+  const [tableRowData, setTableRowData] = useState<any>([]);
 
-  // const handleEditAction = (bool: boolean) => {
-  //   setEditModeActive(bool);
-  //   setModalType("edit");
-  // };
+  //to get Packaging Data from database
+  const handleGetPackaging = async () => {
+        const response = await APIRequest("packaging", "GET") ;
+        if (response) {
+          let initialList = [...initialBagTypes];
+          const modList = initialList.map((d: any) => {
+            const list = response.filter((p: any) => {
+              if (d.type.toLowerCase() === p.bag.toLowerCase()) {
+                return p;
+              }
+            });
 
-  // const handleEditToUpdateAction = () => {
-  //   setModalType("update");
-  //   setModalOpen(true);
-  // };
+            return {
+              ...d,
+              list: list,
+            };
+          });
+          setPackagingData([...modList]);
+        }
+      };
+    
+ //to add new or edit the existing row in the table
+  const handleSaveAction = async (data:any) => {
+       let payload = { ...data };
+       payload.currency = "INR";
+       console.log("data here", payload)
 
-  const handleSaveCallback = (payload: any) => {
-    setModalOpen(false);
-    handleSave(payload);
-  };
-  const handleRefreshCalls = () => {
-    handleGetPackagingList();
-  };
-
-  const handleSaveAction = () => {
-    // Implement the handleSaveAction logic here
-    // For example, to save data to the backend
-  };
-
-  const handleGetPackagingList = async () => {
-    const packagingResponse: any = await APIRequest(`packaging`, "GET");
-    if (packagingResponse) {
-      let initialList = [...initialBagTypes];
-      const modList = initialList.map((d: any) => {
-        const list = packagingResponse.filter((p: any) => {
-          if (d.type.toLowerCase() === p.bag.toLowerCase()) {
-            return p;
-          }
-        });
-
-        return {
-          ...d,
-          list: list,
-        };
-      });
-      setPackagingList([...modList]);
-    }
-  };
-
-  useEffect(() => {
-    handleGetPackagingList();
-  }, []);
-
-  const handleSave = async (payload:any) => {
-    let obj = { ...payload };
-    obj.currency = "INR";
+       if (payload && modalType === "add") {
+        const response = await postPackagingData(payload);
   
-    try {
-      const method = modalType === "add" ? "POST" : "PATCH";
-      const response = await APIRequest("packaging", method, obj);
-  
+        if (response) {
+          handleRefreshCalls();
+          showNotification({
+            title: "Packaging Charges Added!",
+            message: "",
+            autoClose: 2000,
+            icon: <Check />,
+            color:'green',
+          });   
+        }
+      }
+
+    if (payload && modalType === "update") {
+      const response = await putPackagingData(payload);
+
       if (response) {
         handleRefreshCalls();
+        showNotification({
+          title: "Packaging Charges Updated!",
+          message: "",
+          autoClose: 2000,
+          icon: <Check />,
+          color:'green',
+        });   
       }
-    } catch (error) {
-      console.error("Error while saving:", error);
     }
   };
 
-  const handleDeleteVariant = async (data: any) => {
-    // const deleteVariantResponse = await APIRequest(
-    //   "packaging" + "/" + data.packaging + "/" + data._id,
-    //   "DELETE"
-    // );
-
-    // if (deleteVariantResponse) {
-    //   handleRefreshCalls();
-    // }
-  };
-
+  //to delete a single row data
   const openDeleteModal = (rowData: any) =>
     openConfirmModal({
       title: "Delete the Packaging Data",
       centered: true,
       children: (
         <Text size="sm">
-          Are you sure you want to delete the packaging detail? 
+          Are you sure you want to delete the Packaging Data? 
           <Text fw={500}>Note:This action is destructive and you will have to contact support to restore
           this data.</Text> 
           </Text>
@@ -165,49 +152,72 @@ function ManagePackageContainer() {
       labels: { confirm: "Delete Packaging Data", cancel: "No, don't delete it" },
       confirmProps: { color: "red" },
       onCancel: () => console.log("Cancel"),
-      onConfirm: () => handleDeleteVariant(rowData),
+      onConfirm: () => handleDeleteData(rowData),
     });
+    
+  const handleDeleteData = async (data: any) => {
+    const response = await deletePackagingData(data);
 
-  React.useEffect(() => {
-    if (packagingList.length) {
-      let tableData: any = [];
-
-      [...packagingList].forEach((d: any) => {
-        d.list.forEach((l: any) => {
-          const obj = {
-            ...l,
-          };
-          tableData.push(obj);
-        });
+    if (response) {
+      handleRefreshCalls();
+      showNotification({
+        title: "Packaging Charges Deleted!",
+        message: "",
+        autoClose: 2000,
+        icon: <X />,
+        color:'red',
       });
+    }  
+  };
 
-      setTableRowData(tableData);
-    }
-  }, [packagingList]);
+  const handleRefreshCalls = () => {
+    handleGetPackaging();
+  };
+
+  useEffect(() => {
+    handleGetPackaging();
+  }, []);
+
+  useEffect(() => {
+        if (packagingData.length) {
+          let tableData: any = [];
+          [...packagingData].forEach((d: any) => {
+            d.list.forEach((l: any) => {
+              const obj = {
+                ...l,
+              };
+              tableData.push(obj);
+            });
+          });
+    
+          setTableRowData(tableData);
+        }
+      }, [packagingData]);
 
   return (
     <PageWrapper
       PageHeader={() => null}
       PageAction={() => null}
       modalOpen={modalOpen}
-      modalTitle={modalType === "add" ? "Add Packaging Charges" : "Update Packaging Charges"}
-    onModalClose={() => {
-      setModalOpen(false);
-      setUpdateFormData(null);
-      setSelectedTableRowIndex(null);
-    }}
-    ModalContent={() => {
-      return (
-        <RenderModalContent
-          handleCloseModal={(bool: boolean) => setModalOpen(bool)}
-          handleSaveCallback={handleSaveCallback}
-          // variantsData={selectedVariantData}
-          updateFormData={updateFormData}
-          modalType={modalType}
-          modalOpen={modalOpen}
-        />
-      );
-    }}
+      modalTitle={
+        modalType === "add" ? "Add Packaging Charges" : "Update Packaging Charges"
+      }
+      onModalClose={() => {
+        setModalOpen(false)
+        setUpdateFormData(null);
+      }}
+
+      ModalContent={() => {
+          return (
+            <RenderModalContent
+              handleCloseModal={(bool: boolean) => setModalOpen(bool)}
+              handleSaveAction={handleSaveAction}
+              updateFormData={updateFormData}
+              modalType={modalType}
+              modalOpen={modalOpen}
+            />
+          );
+      }}
       modalSize="70%"
     >
       <DataTable
@@ -226,23 +236,22 @@ function ManagePackageContainer() {
           },
         ]}
         handleRowEdit={(row: any, index: number) => {
-          let obj = { ...row };
-          setSelectedTableRowIndex(index);
-
-          const formObj = {
-            bag: obj.bag,
-            weight: obj.weight,
-            cost:obj.cost,
-          };
-
-          setUpdateFormData(formObj);
-          setModalType("update");
-          setModalOpen(true);
-        }}
-        handleRowDelete={(row: any) => {
-          openDeleteModal(row);
-        }}
-      />
+                    let obj = { ...row };
+                    setSelectedTableRowIndex(index);
+                    const formObj = {
+                      bag: obj.bag,
+                      weight: obj.weight,
+                      cost:obj.cost,
+                      _id: obj._id,
+                    };
+                    setUpdateFormData(formObj);
+                    setModalType("update");
+                    setModalOpen(true);
+                  }}
+                  handleRowDelete={(row: any) => {
+                    openDeleteModal(row);
+                  }}
+                />
     </PageWrapper>
   );
 }
