08d9cf15d65c22219237b0f5d0fca3cce9863162
Ananya Singh <ananya@pwip.co>
2024-01-31 05:11:27 +0530

    first column fixed, and serialNumber done

--- src/components/ReactTable/ReactTable.tsx
+++ src/components/ReactTable/ReactTable.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useMemo } from 'react';
 import { useTable, Column, useFilters, usePagination, useSortBy, TableState } from 'react-table';
 import { SortAscending, SortDescending, Pencil, Trash } from 'tabler-icons-react';
-import {
-    ActionIcon, ScrollArea,
-  } from "@mantine/core";
+import { ActionIcon, ScrollArea } from "@mantine/core";
 
 interface TableColumns {
   Header: string;
@@ -40,7 +38,6 @@ const evenRowStyle = {
 
 const ReactTable: React.FC<{ columns: readonly Column<any>[]; data: any[]; onEditRow: (row: any) => void; onDeleteRow: (row: any) => void }> = ({ columns, data, onEditRow, onDeleteRow }) => {
   const [filters, setFilters] = useState<Record<string, any>[]>([]);
-  const [pageIndex, setPageIndex] = useState(0);
 
   const {
     getTableProps,
@@ -107,91 +104,101 @@ const ReactTable: React.FC<{ columns: readonly Column<any>[]; data: any[]; onEdi
           ))
         ))}
       </div>
-<ScrollArea>
-      <table {...getTableProps()} style={tableStyle} className="table">
-        <thead>
-          {headerGroups.map((headerGroup: any, headerGroupIndex: number) => (
-            <tr {...headerGroup.getHeaderGroupProps()} key={headerGroupIndex}>
-              {headerGroup.headers.map((column: any, columnIndex: number) => (
-                <th {...column.getHeaderProps(column.sortable ? column.getSortByToggleProps() : {})}
-                style={{
-                    width: column.width,
-                    position: (columnIndex === 0 || columnIndex === headerGroup.headers.length - 1) ? 'sticky' : 'relative',
-                    left: (columnIndex === 0) ? 0 : 'auto',
-                    right: (columnIndex === headerGroup.headers.length - 1) ? 0 : 'auto',
-                    zIndex: (columnIndex === 0 || columnIndex === headerGroup.headers.length - 1) ? 1 : 'auto',
-                    backgroundColor: (columnIndex === 0 || columnIndex === headerGroup.headers.length - 1) ? '#f8f9fa' : 'transparent',
-                  }}  >               
-                   <span style={{ display: 'flex', alignItems: 'center' }}>
-                    <span>{column.render('Header')}</span>
-                    {column.sortable && (
-                      <span
-                        style={{
-                          marginLeft: '4px',
-                          marginTop: '4px',
-                          color: 'gray',
-                          cursor: 'pointer',
-                        }}
-                      >
-                        {column.isSorted ? (
-                          column.isSortedDesc ? <SortDescending size={20} /> : <SortAscending size={20} />
-                        ) : <SortAscending size={20}/>}
-                      </span>
-                    )}
-                  </span>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {filteredRows.map((row: any, i: any) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()} style={i % 2 === 0 ? evenRowStyle : {}}>
-                {row.cells.map((cell: any, cellIndex: number) => {
-                  if (cell.column.id === 'action') {
-                    return (
-                        <th
-                        {...cell.getCellProps()}
-                        style={{
-                          ...cellStyle,
-                          backgroundColor:'#f8f9fa',
-                          display: 'flex',
-                          alignItems: 'center', 
-                          justifyContent:'center',
-                          position: (cellIndex === 0 || cellIndex === row.cells.length - 1) ? 'sticky' : 'relative',
-                          left: (cellIndex === 0) ? 0 : 'auto',
-                          right: (cellIndex === row.cells.length - 1) ? 0 : 'auto',
-                          zIndex: (cellIndex === 0 || cellIndex === row.cells.length - 1) ? 1 : 'auto'
-                        }}
-                      >
-                        <ActionIcon
-                          variant="default"
-                          onClick={() => onEditRow(row.original)}
-                          style={{ cursor: 'pointer', marginRight: '8px' }}
+      <ScrollArea>
+        <table {...getTableProps()} style={tableStyle} className="table">
+          <thead>
+            {headerGroups.map((headerGroup: any, headerGroupIndex: number) => (
+              <tr {...headerGroup.getHeaderGroupProps()} key={headerGroupIndex}>
+                {headerGroup.headers.map((column: any, columnIndex: number) => (
+                  <th {...column.getHeaderProps(column.sortable ? column.getSortByToggleProps() : {})}
+                    style={{
+                      width: column.width,
+                      position: (columnIndex === 0 || columnIndex === headerGroup.headers.length - 1) ? 'sticky' : 'relative',
+                      left: (columnIndex === 0) ? 0 : 'auto',
+                      right: (columnIndex === headerGroup.headers.length - 1) ? 0 : 'auto',
+                      zIndex: (columnIndex === 0 || columnIndex === headerGroup.headers.length - 1) ? 1 : 'auto',
+                       backgroundColor: (columnIndex === 0) ? '#f8f9fa' : ((columnIndex === headerGroup.headers.length - 1) ? '#f8f9fa' : 'transparent'),
+                    }}  >
+                    <span style={{ display: 'flex', alignItems: 'center' }}>
+                      <span>{column.render('Header')}</span>
+                      {column.sortable && (
+                        <span
+                          style={{
+                            marginLeft: '4px',
+                            marginTop: '4px',
+                            color: 'gray',
+                            cursor: 'pointer',
+                          }}
                         >
-                          <Pencil size="1rem" color="green" />
-                        </ActionIcon>
-                      
-                        <ActionIcon
-                          variant="default"
-                          onClick={() => onDeleteRow(row.original)}
-                          style={{ cursor: 'pointer' }}
-                        >
-                          <Trash size="1rem" color="red" />
-                        </ActionIcon>
-                      </th>
-                    );
-                  } else {
-                    return <td {...cell.getCellProps()} style={cellStyle}>{cell.render('Cell')}</td>;
-                  }
-                })}
+                          {column.isSorted ? (
+                            column.isSortedDesc ? <SortDescending size={20} /> : <SortAscending size={20} />
+                          ) : <SortAscending size={20}/>}
+                        </span>
+                      )}
+                    </span>
+                  </th>
+                ))}
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {filteredRows.map((row: any, i: any) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()} style={i % 2 === 0 ? evenRowStyle : {}}>
+                  {row.cells.map((cell: any, cellIndex: number) => {
+
+                    if (cell.column.id === 'action') {
+                      return (
+                          <th
+                          {...cell.getCellProps()}
+                          style={{
+                            ...cellStyle,
+                            backgroundColor:'#f8f9fa',
+                            display: 'flex',
+                            alignItems: 'center', 
+                            justifyContent:'center',
+                            position: (cellIndex === row.cells.length - 1) ? 'sticky' : 'relative',
+                            right: (cellIndex === row.cells.length - 1) ? 0 : 'auto',
+                            zIndex: (cellIndex === row.cells.length - 1) ? 1 : 'auto'
+                          }}
+                        >
+                          <ActionIcon
+                            variant="default"
+                            onClick={() => onEditRow(row.original)}
+                            style={{ cursor: 'pointer', marginRight: '8px' }}
+                          >
+                            <Pencil size="1rem" color="green" />
+                          </ActionIcon>
+                        
+                          <ActionIcon
+                            variant="default"
+                            onClick={() => onDeleteRow(row.original)}
+                            style={{ cursor: 'pointer' }}
+                          >
+                            <Trash size="1rem" color="red" />
+                          </ActionIcon>
+                        </th>
+                      );
+                    } else if (cell.column.id === 'serialNo') {
+                      return <td {...cell.getCellProps()}  style={{
+                        ...cellStyle,
+                        backgroundColor:'#f8f9fa',
+                        position: (cellIndex === 0) ? 'sticky' : 'relative',
+                        left: (cellIndex === 0) ? 0 : 'auto',
+                        zIndex: (cellIndex === 0) ? 1 : 'auto'
+                      }}>
+                        {(currentPage * page.length) + i + 1}
+                        </td>;
+                    } else {
+                      return <td {...cell.getCellProps()} style={cellStyle}>{cell.render('Cell')}</td>;
+                    }
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </ScrollArea>
 
       <div style={{ marginTop: '15px' }}>
@@ -204,7 +211,7 @@ const ReactTable: React.FC<{ columns: readonly Column<any>[]; data: any[]; onEdi
         <span>
           Page{' '}
           <strong>
-            {pageIndex + 1} of {pageOptions.length}
+            {currentPage + 1} of {pageOptions.length}
           </strong>{' '}
         </span>
       </div>
